9d6497644207b3ed510967294817206662844fc9
Pai Ge <fengdemeng@me.com>
2019-05-13 23:12:07 -0700

    finish part iii

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,19 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int = when {
+        year != other.year -> year - other.year
+        month != other.month -> month - other.month
+        else -> dayOfMonth - other.dayOfMonth
+    }
+}
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
+
+operator fun MyDate.plus(interval: TimeInterval) = addTimeIntervals(interval, 1)
+operator fun MyDate.plus(repeatedTimeInternal: RepeatedTimeInternal) = addTimeIntervals(repeatedTimeInternal.interval, repeatedTimeInternal.times)
+
+operator fun TimeInterval.times(times : Int) = RepeatedTimeInternal(this, times)
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +21,18 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(override val start: MyDate, override val endInclusive: MyDate) : ClosedRange<MyDate>, Iterable<MyDate> {
+    override fun iterator() = object: Iterator<MyDate> {
+        var current = start
+
+        override fun hasNext() = current <= endInclusive
+
+        override fun next(): MyDate {
+            val result = current
+            current = current.nextDay()
+            return result
+        }
+    }
+}
+
+data class RepeatedTimeInternal(val interval: TimeInterval, val times: Int)
